5864f1994784bab4a5a453c72d87c7c86eeb37a7
jsberlanga <v-jusoto@microsoft.com>
2019-02-04 19:13:32 +0100

    es6-destructuring

---
+++ src/playground/es6-destructuring.js.js
@@ -0,0 +1,43 @@
+//
+// Object destructuring
+//
+
+// const person = {
+//   name: 'Julio',
+//   age: 31,
+//   location: {
+//     city: 'Krakow',
+//     temp: 12
+//   }
+// };
+
+// const { name: firstName = 'Anonymous', age } = person;
+// const { city, temp: temperature} = person.location;
+
+// console.log(`${firstName} is ${age} years old.`);
+// console.log(`It's ${temperature} in ${city}.`);
+
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// };
+
+// const { name: publisherName = 'Self-Published'} = book.publisher;
+// console.log(publisherName);
+
+//
+// Array Destructuring
+//
+
+const address = ['Dobrego Pasterza 120', 'Krakow', , '31-416'];
+const [, city, region = 'Malopolska'] = address;
+
+console.log(`You are in ${city} ${region}`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [drink, small, medium, large] = item;
+console.log(`A Medium ${drink} costs ${medium}`)
