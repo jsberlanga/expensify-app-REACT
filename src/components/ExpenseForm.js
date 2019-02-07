063438c287852472be4ddf84b2815cb3b6cf281b
jsberlanga <v-jusoto@microsoft.com>
2019-02-07 16:18:45 +0100

    add-edit form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -2,11 +2,25 @@ import React from 'react';
 
 export default class ExpenseForm extends React.Component {
     state = {
-        description: ''
+        description: '',
+        note: '',
+        amount: ''
     };
     onDescriptionChange = (e) => {
         const description = e.target.value;
         this.setState(() => ({ description }));
+    };
+    onNoteChange = (e) => {
+        const note = e.target.value;
+        this.setState(() => ({ note }))
+    };
+    onAmountChange = (e) => {
+        const amount = e.target.value;
+
+        if (amount.match(/^\d{0,6}(\.\d{0,2})?$/)) {
+
+            this.setState(() => ({ amount }))
+        }
     };
     render() {
         return (
@@ -20,11 +34,15 @@ export default class ExpenseForm extends React.Component {
                         onChange={this.onDescriptionChange}
                     />
                     <input
-                        type="number"
+                        value={this.state.amount}
+                        onChange={this.onAmountChange}
+                        type="text"
                         placeholder="Amount"
                     />
                     <textarea
+                        value={this.state.note}
                         placeholder="Add a note for your expense (optional)."
+                        onChange={this.onNoteChange}
                     >
                     </textarea>
                     <button>Add Expense</button>
