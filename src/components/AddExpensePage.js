50637650189e359791d24e4c7a5e40f543410c15
jsberlanga <v-jusoto@microsoft.com>
2019-02-07 22:11:29 +0100

    edit expense

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { addExpense } from '../actions/expenses';
+import { addExpense } from '../actions/expenses'
 
 const AddExpensePage = (props) => (
     <div>
@@ -9,7 +9,6 @@ const AddExpensePage = (props) => (
         <ExpenseForm
             onSubmit={(expense) => {
                 props.dispatch(addExpense(expense));
-                props.history.push('/');
             }}
         />
     </div>
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -2,16 +2,19 @@ import React from 'react';
 import moment from 'moment';
 import { SingleDatePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
-// import 'react-dates/initialize';
+
 
 export default class ExpenseForm extends React.Component {
-    state = {
-        description: '',
-        note: '',
-        amount: '',
-        createdAt: moment(),
-        calendarFocused: false,
-        error: ''
+    constructor(props) {
+        super(props)
+        this.state = {
+            description: props.expense ? props.expense.description : '',
+            note: props.expense ? props.expense.note : '',
+            amount: props.expense ? (props.expense.amount.toString() / 100) : '',
+            createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+            calendarFocused: false,
+            error: ''
+        }
     };
     onDescriptionChange = (e) => {
         const description = e.target.value;
